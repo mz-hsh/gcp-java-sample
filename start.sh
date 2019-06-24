2d5627ce12fe3d243a5a0532d569fe9ca1002e7c
mz-hsh <hsh@mz.co.kr>
2019-06-24 15:52:48 +0900

    start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # metadata
-BUCKET=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
+export BUCKET=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
 GIT_REPO=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/GIT_REPO" -H "Metadata-Flavor: Google")
+
+echo "BUCKET: ${BUCKET}"
+echo "GIT_REPO: ${GIT_REPO}"
 
 # Install dependencies from apt
 apt-get update
